4b35b01cd5c4e5dcef08c8768eac925566ebea68
channinghsu <xbc1241888704@gmail.com>
2024-03-01 22:37:01 +0800

    update keymaps

--- lua/user/keymap/editor.lua
+++ lua/user/keymap/editor.lua
@@ -1,7 +1,8 @@
 local bind = require("keymap.bind")
 local map_cr = bind.map_cr
+local map_cmd = bind.map_cmd
 
 return {
-	-- Plugin: nvim-bufdel
 	["n|<leader>x"] = map_cr("BufDel"):with_noremap():with_silent():with_desc("buffer: Close current"),
+	["nv|<leader>ww"] = map_cmd("<Cmd>HopWordMW<CR>"):with_noremap():with_desc("jump: Goto word"),
 }
