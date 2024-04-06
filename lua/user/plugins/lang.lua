574f6b160281c61ae34f5342eb2040e6f777686b
channinghsu <xbc1241888704@gmail.com>
2024-04-06 11:44:13 +0800

    feat: Update plugin versions and add dial.nvim configuration
    - Updated versions of multiple plugins in lazy-lock.json
    - Added key mappings and configuration for dial.nvim in lang.lua
    - Added window focus key mappings in editor.lua

--- lua/user/plugins/lang.lua
+++ lua/user/plugins/lang.lua
@@ -1,3 +1,35 @@
 local lang = {}
-
+lang["monaqa/dial.nvim"] = {
+	keys = {
+		{
+			"<C-o>",
+			function()
+				return require("dial.map").inc_normal()
+			end,
+			expr = true,
+			desc = "dial: Increment",
+		},
+		{
+			"<C-i>",
+			function()
+				return require("dial.map").dec_normal()
+			end,
+			expr = true,
+			desc = "dial: Decrement",
+		},
+	},
+	config = function()
+		local augend = require("dial.augend")
+		require("dial.config").augends:register_group({
+			default = {
+				augend.integer.alias.decimal,
+				augend.integer.alias.hex,
+				augend.date.alias["%Y/%m/%d"],
+				augend.constant.alias.bool,
+				augend.semver.alias.semver,
+				augend.constant.new({ elements = { "let", "const" } }),
+			},
+		})
+	end,
+}
 return lang
