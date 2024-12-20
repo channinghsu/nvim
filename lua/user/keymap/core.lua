10cdf22b14f1ccb2ce7b2166ee417d0ac3fd75bf
channinghsu <xbc1241888704@gmail.com>
2024-12-20 09:39:48 +0800

    update: change leader x to close current buffer

--- lua/user/keymap/core.lua
+++ lua/user/keymap/core.lua
@@ -5,7 +5,7 @@ local map_cmd = bind.map_cmd
 return {
 	["n|L"] = map_cr("BufferLineCycleNext"):with_noremap():with_silent():with_nowait():with_desc("buffer: Switch to next"),
 	["n|H"] = map_cr("BufferLineCyclePrev"):with_noremap():with_silent():with_nowait():with_desc("buffer: Switch to prev"),
-	["n|<leader>x"] = map_cr("wq"):with_desc("which_key_ignore"),
+	["n|<leader>x"] = map_cr("BufDel"):with_noremap():with_silent():with_desc("which_key_ignore"),
 	["n|<leader>q"] = map_cr("q!"):with_desc("which_key_ignore"),
 	["n|<leader>i"] = map_cmd("ggVG"):with_noremap():with_silent():with_desc("which_key_ignore"),
 	["i|jk"] = map_cmd("<Esc>:w<CR>"):with_noremap():with_silent():with_nowait():with_desc("edit: Save file"),
