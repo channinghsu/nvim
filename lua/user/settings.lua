f83b35cb12889c81a3b6219960f92c1f498156f6
channinghsu <xbc1241888704@gmail.com>
2024-12-25 19:17:57 +0800

    update: shut down lsp_inlayhints

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -1,16 +1,11 @@
 -- Please check `lua/core/settings.lua` to view the full list of configurable settings
 local settings = {}
-
 settings["use_ssh"] = false
-
 settings["format_notify"] = false
-
 settings["colorscheme"] = "catppuccin"
-
 settings["transparent_background"] = true
-
 settings["format_on_save"] = false
-
+settings["lsp_inlayhints"] = false
 settings["dashboard_image"] = {
 	[[                                                                       ]],
 	[[  ██████   █████                   █████   █████  ███                  ]],
