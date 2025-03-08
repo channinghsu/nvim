fe15542e3875f836e59d873f7d1da59e2dd9fb61
channinghsu <xbc1241888704@gmail.com>
2025-03-08 10:52:51 +0800

    chore: delete user_template and some comments

--- lua/user/configs/editor/better-escape.lua
+++ lua/user/configs/editor/better-escape.lua
@@ -2,15 +2,10 @@ return function()
 	require("modules.utils").load_plugin("better_escape", {
 		timeout = vim.o.timeoutlen,
 		mappings = {
-			-- 插入模式
 			i = { j = { k = "<Esc>" } },
-			-- 命令模式
 			c = { j = { k = "<Esc>" } },
-			-- 终端模式
 			t = { j = { k = "<Esc>" } },
-			-- 可视模式
 			v = { j = { k = "<Esc>" } },
-			-- 选择模式
 			s = { j = { k = "<Esc>" } },
 		},
 	})
