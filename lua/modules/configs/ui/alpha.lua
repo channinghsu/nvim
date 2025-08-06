47e46c6de2d784efcffba40e98d6b72abcb63f1c
channinghsu <xbc1241888704@gmail.com>
2025-08-06 09:59:10 +0800

    udpate: alpha ui

--- lua/modules/configs/ui/alpha.lua
+++ lua/modules/configs/ui/alpha.lua
@@ -95,14 +95,7 @@ return function()
 	local function footer()
 		local stats = require("lazy").stats()
 		local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-		return "   Have Fun with neovim"
-			.. "  󰀨 v"
-			.. vim.version().major
-			.. "."
-			.. vim.version().minor
-			.. "."
-			.. vim.version().patch
-			.. "  󰂖 "
+		return "󰂖 "
 			.. stats.count
 			.. " plugins in "
 			.. ms
