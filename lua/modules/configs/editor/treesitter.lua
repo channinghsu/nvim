a35b64d48b785caf6a20f1ab85532a3e03705d50
channinghsu <xbc1241888704@gmail.com>
2026-01-29 10:25:55 +0800

    fix(treesitter): stop auto installing when nivm start

--- lua/modules/configs/editor/treesitter.lua
+++ lua/modules/configs/editor/treesitter.lua
@@ -1,7 +1,8 @@
-return vim.schedule_wrap(function()
+return function()
 	vim.api.nvim_set_option_value("indentexpr", "v:lua.require'nvim-treesitter'.indentexpr()", {})
 
-	require("modules.utils").load_plugin("nvim-treesitter", {})
-
-	require("nvim-treesitter").install(require("core.settings").treesitter_deps)
-end)
+	-- Setup nvim-treesitter config with proper install directory
+	require("nvim-treesitter").setup({
+		install_dir = vim.fn.stdpath('data') .. '/site',
+	})
+end
